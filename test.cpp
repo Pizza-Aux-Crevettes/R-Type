320e044351cc4d2123cd5af91a889940bbaebbbb
eddygardes <eddy.gardes@epitech.eu>
2024-11-30 20:45:32 +0100

    STYLE: Ensure test file adheres to clang-format
    Added formatting changes to make the test file compliant with the clang-format style.

--- test.cpp
+++ test.cpp
@@ -14,13 +14,15 @@
 
 int add(int a, int b) { return a + b; }
 
-int main() {
+int main()
+{
     int num1 = 10;
     int num2 = 20;
     int result = add(num1, num2);
 
     printf("La somme de %d et %d est %d\n", num1, num2, result);
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         printf("Nombre : %d\n", i);
     }
     return 0;
