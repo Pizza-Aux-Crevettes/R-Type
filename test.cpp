b653f5d4bf8f0efe3dd129bdb4e965684083ee01
eddygardes <eddy.gardes@epitech.eu>
2024-11-30 21:12:21 +0100

    FIX: clang format github action to show errors
    fixe the GitHub Actions workflow to properly show clang-format errors as annotations.

---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x= 10; int y =20;
+    if(x<y){cout<<"x est plus petit que y"<<endl;} else{cout<<"x est plus grand ou égal à y"<<endl;} return 0;
+}
