6405415024ee0bf0b8120477f7f8187204cae81d
eddygardes <eddy.gardes@epitech.eu>
2024-11-30 21:26:16 +0100

    FIX: test file with clang format
    fix test file to be conformed with clang format

--- test.cpp
+++ test.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int x= 10; int y =20;
-    if(x<y){cout<<"x est plus petit que y"<<endl;} else{cout<<"x est plus grand ou égal à y"<<endl;} return 0;
+int main()
+{
+    int x = 10;
+    int y = 20;
+    if (x < y)
+    {
+        cout << "x est plus petit que y" << endl;
+    }
+    else
+    {
+        cout << "x est plus grand ou égal à y" << endl;
+    }
+    return 0;
 }
