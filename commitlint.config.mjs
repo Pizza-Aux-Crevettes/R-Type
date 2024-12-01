417fc9dbcd21d2cf25b395ae4d85534b989fb7d0
eddygardes <eddy.gardes@epitech.eu>
2024-12-01 18:51:06 +0100

    FIX: errors with type of commit
    fix errors with type of commit in the config file

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,6 @@
 export default {
     rules: {
-        'type-enum': [2, 'always', ['ADD', 'FIX', 'UPDATE', 'REMOVE']],
+        'type-enum': [2, 'always', ['ADD', 'FIX', 'DOCS', 'STYLE', 'REFACTOR', 'TEST', 'RM']],
         'type-case': [2, 'always', 'upper-case'],
         'body-min-length': [2, 'always', 10],
     },
