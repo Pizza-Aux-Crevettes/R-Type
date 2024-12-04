bc9728d1384de06a2d00ef132108061a8ade1d14
Anastasia <anastasia.bouby@epitech.eu>
2024-12-04 09:07:46 +0100

    FIX: conflit fix conflict files

---
+++ client/include/Client.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-TLS-5-2-rtype-anastasia.bouby
+** File description:
+** This file declares the `Client` class, responsible for managing the 
+** client's entities and items in the game context. It allows for the 
+** creation, updating, and drawing of items as well as setting their 
+** properties like position, text, and sprites.
+** Client.hpp
+*/
+
+#pragma once
+
+#include <iostream>
+#include <any>
+
+class Client {
+    private: 
+        std::vector<std::any> _items;
+
+    public:
+        Client();
+        ~Client();
+        void manageClient();
+        void setItems(std::vector<std::any> items);
+        std::vector<std::any> getItems();
+        void listenServer();
+};
