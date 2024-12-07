09150a2d7046608587fc1dbc43b854459afab556
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-12-08 00:53:55 +0100

    ADD: Point.cpp and Point.hpp

---
+++ network/src/util/Point.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-TLS-5-2-rtype-anastasia.bouby
+** File description:
+** Point.cpp
+*/
+
+#include "util/Point.hpp"
+
+Point::Point(double x, double y) : _x(x), _y(y) {}
+
+double Point::getX() const {
+    return _x;
+}
+
+double Point::getY() const {
+    return _y;
+}
+
+void Point::setX(double x) {
+    _x = x;
+}
+
+void Point::setY(double y) {
+    _y = y;
+}
