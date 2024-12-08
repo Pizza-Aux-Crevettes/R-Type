62e5386f6fb939e4f010f23ed01d07e441748222
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-12-08 01:39:35 +0100

    CHORE: Room and RoomManager classes Logger implementation

--- network/src/component/room/Room.cpp
+++ network/src/component/room/Room.cpp
@@ -6,32 +6,51 @@
 */
 
 #include "component/room/Room.hpp"
+#include "util/Logger.hpp"
 
 Room::Room(int id, const std::string& code, size_t capacity)
-    : _id(id), _code(code), _capacity(capacity) {}
+    : _id(id), _code(code), _capacity(capacity) {
+    Logger::info("[Room] Created room with ID: " + std::to_string(id) +
+                 ", Code: " + code + ", Capacity: " + std::to_string(capacity));
+}
 
 int Room::getId() const {
+    Logger::debug("[Room] Accessed ID: " + std::to_string(_id));
+
     return _id;
 }
 
 const std::string& Room::getCode() const {
+    Logger::debug("[Room] Accessed Code: " + _code);
+
     return _code;
 }
 
 size_t Room::getCapacity() const {
+    Logger::debug("[Room] Accessed Capacity: " + std::to_string(_capacity));
+
     return _capacity;
 }
 
 const std::vector<std::shared_ptr<Player>>& Room::getPlayers() const {
+    Logger::debug("[Room] Accessed list of players in Room ID: " +
+                  std::to_string(_id));
+
     return _players;
 }
 
 bool Room::addPlayer(const std::shared_ptr<Player>& player) {
     if (_players.size() >= _capacity) {
+        Logger::warning("[Room] Cannot add player: " + player->getName() +
+                        ". Room ID: " + std::to_string(_id) + " is full.");
+
         return false;
     }
 
     _players.push_back(player);
+
+    Logger::info("[Room] Player " + player->getName() +
+                 " added to Room ID: " + std::to_string(_id));
 
     return true;
 }
@@ -45,8 +64,15 @@ bool Room::removePlayer(const std::string& playerName) {
 
     if (it != _players.end()) {
         _players.erase(it, _players.end());
+
+        Logger::info("[Room] Player " + playerName +
+                     " removed from Room ID: " + std::to_string(_id));
+
         return true;
     }
+
+    Logger::warning("[Room] Player " + playerName +
+                    " not found in Room ID: " + std::to_string(_id));
 
     return false;
 }
--- network/src/component/room/RoomManager.cpp
+++ network/src/component/room/RoomManager.cpp
@@ -6,21 +6,33 @@
 */
 
 #include "component/room/RoomManager.hpp"
+#include "util/Logger.hpp"
 
 RoomManager& RoomManager::getInstance() {
     static RoomManager instance;
+
+    Logger::info("[RoomManager] Instance accessed.");
+
     return instance;
 }
 
-RoomManager::RoomManager() : _nextRoomId(1) {}
+RoomManager::RoomManager() : _nextRoomId(1) {
+    Logger::info("[RoomManager] Initialized RoomManager.");
+}
 
-RoomManager::~RoomManager() {}
+RoomManager::~RoomManager() {
+    Logger::info("[RoomManager] Destroyed RoomManager.");
+}
 
 std::shared_ptr<Room> RoomManager::createRoom(const std::string& code,
                                               size_t capacity) {
     auto room = std::make_shared<Room>(_nextRoomId++, code, capacity);
 
     _rooms.push_back(room);
+
+    Logger::success(
+        "[RoomManager] Created room with ID: " + std::to_string(room->getId()) +
+        ", Code: " + code + ", Capacity: " + std::to_string(capacity) + ".");
 
     return room;
 }
@@ -33,8 +45,15 @@ bool RoomManager::deleteRoom(int roomId) {
 
     if (it != _rooms.end()) {
         _rooms.erase(it, _rooms.end());
+
+        Logger::success("[RoomManager] Deleted room with ID: " +
+                        std::to_string(roomId) + ".");
+
         return true;
     }
+
+    Logger::warning("[RoomManager] Failed to delete room with ID: " +
+                    std::to_string(roomId) + ".");
 
     return false;
 }
@@ -42,9 +61,15 @@ bool RoomManager::deleteRoom(int roomId) {
 std::shared_ptr<Room> RoomManager::findRoomById(int roomId) {
     for (auto& room : _rooms) {
         if (room->getId() == roomId) {
+            Logger::info("[RoomManager] Found room with ID: " +
+                         std::to_string(roomId) + ".");
+
             return room;
         }
     }
+
+    Logger::warning("[RoomManager] Room with ID: " + std::to_string(roomId) +
+                    " not found.");
 
     return nullptr;
 }
@@ -52,13 +77,19 @@ std::shared_ptr<Room> RoomManager::findRoomById(int roomId) {
 std::shared_ptr<Room> RoomManager::findRoomByCode(const std::string& code) {
     for (auto& room : _rooms) {
         if (room->getCode() == code) {
+            Logger::info("[RoomManager] Found room with Code: " + code + ".");
+
             return room;
         }
     }
+
+    Logger::warning("[RoomManager] Room with Code: " + code + " not found.");
 
     return nullptr;
 }
 
 const std::vector<std::shared_ptr<Room>>& RoomManager::getRooms() const {
+    Logger::debug("[RoomManager] Accessed list of all rooms.");
+
     return _rooms;
 }
