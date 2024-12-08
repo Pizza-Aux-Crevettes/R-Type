446e0cd2a337926e5c5cf1893502b2651baf5313
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-12-08 01:39:23 +0100

    CHORE: Point class Logger implementation

--- network/src/util/Point.cpp
+++ network/src/util/Point.cpp
@@ -6,21 +6,35 @@
 */
 
 #include "util/Point.hpp"
+#include "util/Logger.hpp"
 
-Point::Point(double x, double y) : _x(x), _y(y) {}
+Point::Point(double x, double y) : _x(x), _y(y) {
+    Logger::info("[Point] Created Point at (" + std::to_string(x) + ", " +
+                 std::to_string(y) + ")");
+}
 
 double Point::getX() const {
+    Logger::debug("[Point] Accessed X: " + std::to_string(_x));
+
     return _x;
 }
 
 double Point::getY() const {
+    Logger::debug("[Point] Accessed Y: " + std::to_string(_y));
+
     return _y;
 }
 
 void Point::setX(double x) {
+    Logger::info("[Point] Updated X from " + std::to_string(_x) + " to " +
+                 std::to_string(x));
+
     _x = x;
 }
 
 void Point::setY(double y) {
+    Logger::info("[Point] Updated Y from " + std::to_string(_y) + " to " +
+                 std::to_string(y));
+
     _y = y;
 }
