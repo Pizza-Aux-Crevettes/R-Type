90aef16701a3c471da68c67ccb8f74d47894fa2b
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-12-10 09:51:25 +0100

    REFACTOR: CppDoc on the Protocol.cpp

--- network/src/protocol/Protocol.cpp
+++ network/src/protocol/Protocol.cpp
@@ -5,19 +5,42 @@
 ** Protocol.cpp
 */
 
+/**
+ * @file Protocol.cpp
+ * @brief Implementation of the Protocol class for handling different operation
+ * codes (OpCodes).
+ */
+
 #include "protocol/Protocol.hpp"
+#include "component/player/PlayerProtocol.hpp"
 #include "component/room/RoomProtocol.hpp"
 #include "util/Logger.hpp"
 
+/**
+ * @brief Retrieves the singleton instance of the Protocol.
+ * @return The singleton instance of the Protocol.
+ */
 Protocol& Protocol::getInstance() {
     static Protocol instance;
     return instance;
 }
 
+/**
+ * @brief Constructs the Protocol. Currently does nothing.
+ */
 Protocol::Protocol() {}
 
+/**
+ * @brief Destructs the Protocol. Currently does nothing.
+ */
 Protocol::~Protocol() {}
 
+/**
+ * @brief Handles an incoming message by dispatching it to the appropriate
+ * handler based on the OpCode.
+ * @param clientSocket The client socket that sent the message.
+ * @param smartBuffer The buffer containing the message data.
+ */
 void Protocol::handleMessage(int clientSocket, SmartBuffer& smartBuffer) {
     int16_t opCode;
     smartBuffer >> opCode;
@@ -26,14 +49,30 @@ void Protocol::handleMessage(int clientSocket, SmartBuffer& smartBuffer) {
 
     switch (opCode) {
     case CREATE_ROOM:
+        Logger::trace("[Protocol] Dispatching to RoomProtocol::createRoom");
+
         RoomProtocol::createRoom(clientSocket, smartBuffer);
         break;
+
     case JOIN_ROOM:
+        Logger::trace("[Protocol] Dispatching to RoomProtocol::joinRoom");
+
         RoomProtocol::joinRoom(clientSocket, smartBuffer);
         break;
+
     case DELETE_ROOM:
+        Logger::trace("[Protocol] Dispatching to RoomProtocol::deleteRoom");
+
         RoomProtocol::deleteRoom(clientSocket, smartBuffer);
         break;
+
+    case NEW_PLAYER:
+        Logger::trace(
+            "[Protocol] Dispatching to PlayerProtocol::createOrGetPlayer");
+
+        PlayerProtocol::newPlayer(clientSocket, smartBuffer);
+        break;
+
     default:
         Logger::error("[Protocol] Received unknown OpCode: " +
                       std::to_string(opCode));
