07a13b173edd97e62861bd80a5910be000668aab
fohz67 <benjamin.gayaud@epitech.eu>
2024-12-12 00:25:36 +0100

    FIX: RoomProtocol : code improvement and linting

--- network/include/component/room/RoomProtocol.hpp
+++ network/include/component/room/RoomProtocol.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <SmartBuffer.hpp>
-#include <string>
 
 class RoomProtocol {
   public:
--- network/src/component/room/RoomProtocol.cpp
+++ network/src/component/room/RoomProtocol.cpp
@@ -6,41 +6,43 @@
 */
 
 #include "component/room/RoomProtocol.hpp"
+#include "component/player/PlayerManager.hpp"
+#include "component/room/RoomManager.hpp"
 #include "protocol/Protocol.hpp"
 #include "socket/TcpSocket.hpp"
 #include "util/Logger.hpp"
-#include "util/Singletons.hpp"
 
 /**
  * Protocol structure:
- * - Input: int32_t playerId >> int16_t capacity >> int16_t isPublic
+ * - Input: int32_t userId >> int16_t capacity >> int16_t isPublic
  * - Output: int16_t opCode (CREATE_ROOM_CALLBACK) << int16_t status
  *
  * Status codes:
  * - 0 = Room created
  * - 1 = Player not found
  */
-void RoomProtocol::createRoom(int clientSocket, SmartBuffer& smartBuffer) {
-    int32_t playerId;
+void RoomProtocol::createRoom(const int clientSocket,
+                              SmartBuffer& smartBuffer) {
+    int32_t userId;
     int16_t capacity;
     int16_t isPublic;
 
-    smartBuffer >> playerId >> capacity >> isPublic;
+    smartBuffer >> userId >> capacity >> isPublic;
     smartBuffer.reset();
     smartBuffer << static_cast<int16_t>(Protocol::OpCode::CREATE_ROOM_CALLBACK);
 
-    Logger::trace("[RoomProtocol] Processing CREATE_ROOM. playerId = " +
-                  std::to_string(playerId) +
+    Logger::trace("[RoomProtocol] Processing CREATE_ROOM. userId = " +
+                  std::to_string(userId) +
                   ", capacity = " + std::to_string(capacity) +
                   ", isPublic = " + std::to_string(isPublic));
 
-    const auto player = Singletons::getPlayerManager().findPlayerById(playerId);
+    const auto player = PlayerManager::getInstance().findPlayerById(userId);
     const auto status = static_cast<int16_t>(!player);
 
     smartBuffer << status;
 
     if (!status) {
-        const std::string roomCode = Singletons::getRoomManager()
+        const std::string roomCode = RoomManager::getInstance()
                                          .createRoom(player, capacity, isPublic)
                                          ->getCode();
 
@@ -53,7 +55,7 @@ void RoomProtocol::createRoom(int clientSocket, SmartBuffer& smartBuffer) {
 
 /**
  * Protocol structure:
- * - Input: std::string roomCode >> int32_t playerId
+ * - Input: std::string roomCode >> int32_t userId
  * - Output: int16_t opCode (JOIN_ROOM_CALLBACK) << int16_t status
  *
  * Status codes:
@@ -62,21 +64,21 @@ void RoomProtocol::createRoom(int clientSocket, SmartBuffer& smartBuffer) {
  * - 2 = Room not found
  * - 3 = Cannot add player to the room
  */
-void RoomProtocol::joinRoom(int clientSocket, SmartBuffer& smartBuffer) {
+void RoomProtocol::joinRoom(const int clientSocket, SmartBuffer& smartBuffer) {
     std::string roomCode;
-    int32_t playerId;
+    int32_t userId;
 
-    smartBuffer >> roomCode >> playerId;
+    smartBuffer >> roomCode >> userId;
     smartBuffer.reset();
     smartBuffer << static_cast<int16_t>(Protocol::OpCode::JOIN_ROOM_CALLBACK);
 
     Logger::trace("[RoomProtocol] Processing JOIN_ROOM. roomCode = " +
-                  roomCode + ", playerId = " + std::to_string(playerId));
+                  roomCode + ", userId = " + std::to_string(userId));
 
-    const auto player = Singletons::getPlayerManager().findPlayerById(playerId);
-    const auto room = Singletons::getRoomManager().findRoomByCode(roomCode);
+    const auto player = PlayerManager::getInstance().findPlayerById(userId);
+    const auto room = RoomManager::getInstance().findRoomByCode(roomCode);
     const auto status =
-        static_cast<int16_t>(!player + !room + (!room->addPlayer(player)));
+        static_cast<int16_t>(!player + !room + !room->addPlayer(player));
 
     smartBuffer << status;
 
@@ -90,7 +92,7 @@ void RoomProtocol::joinRoom(int clientSocket, SmartBuffer& smartBuffer) {
 
 /**
  * Protocol structure:
- * - Input: std::string roomCode >> int32_t playerId
+ * - Input: std::string roomCode >> int32_t userId
  * - Output: P
  *
  * Status codes:
@@ -99,26 +101,27 @@ void RoomProtocol::joinRoom(int clientSocket, SmartBuffer& smartBuffer) {
  * - 2 = Room not found
  * - 3 = Permission not granted
  */
-void RoomProtocol::deleteRoom(int clientSocket, SmartBuffer& smartBuffer) {
+void RoomProtocol::deleteRoom(const int clientSocket,
+                              SmartBuffer& smartBuffer) {
     std::string roomCode;
-    int32_t playerId;
+    int32_t userId;
 
-    smartBuffer >> roomCode >> playerId;
+    smartBuffer >> roomCode >> userId;
     smartBuffer.reset();
     smartBuffer << static_cast<int16_t>(Protocol::OpCode::DELETE_ROOM_CALLBACK);
 
     Logger::trace("[RoomProtocol] Processing DELETE_ROOM. roomCode = " +
-                  roomCode + ", playerId = " + std::to_string(playerId));
+                  roomCode + ", userId = " + std::to_string(userId));
 
-    const auto player = Singletons::getPlayerManager().findPlayerById(playerId);
-    const auto room = Singletons::getRoomManager().findRoomByCode(roomCode);
+    const auto player = PlayerManager::getInstance().findPlayerById(userId);
+    const auto room = RoomManager::getInstance().findRoomByCode(roomCode);
     const auto status =
         static_cast<int16_t>(!player + !room + (room->getOwner() != player));
 
     smartBuffer << status;
 
     if (!status) {
-        Singletons::getRoomManager().deleteRoom(roomCode, player);
+        RoomManager::getInstance().deleteRoom(roomCode, player);
 
         Logger::info("[RoomProtocol] Room deleted successfully. Room code: " +
                      roomCode);
