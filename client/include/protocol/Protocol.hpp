bc75ab184ff685b86d560535ad7ed2ad619e574a
fohz67 <benjamin.gayaud@epitech.eu>
2024-12-15 12:56:14 +0100

    FIX: clang

--- client/include/protocol/Protocol.hpp
+++ client/include/protocol/Protocol.hpp
@@ -10,42 +10,42 @@
 #include <SmartBuffer.hpp>
 
 class Protocol {
-public:
-  enum OpCode {
-      DEFAULT,
-      CREATE_ROOM,
-      CREATE_ROOM_CALLBACK,
-      CREATE_ROOM_BROADCAST,
-      JOIN_ROOM,
-      JOIN_ROOM_CALLBACK,
-      JOIN_ROOM_BROADCAST,
-      DELETE_ROOM,
-      DELETE_ROOM_CALLBACK,
-      DELETE_ROOM_BROADCAST,
-      NEW_PLAYER,
-      NEW_PLAYER_CALLBACK,
-      NEW_PLAYER_BROADCAST,
-      PLAYER_UPDATE_POSITION,
-      HOTKEY_PRESSED
-};
+  public:
+    enum OpCode {
+        DEFAULT,
+        CREATE_ROOM,
+        CREATE_ROOM_CALLBACK,
+        CREATE_ROOM_BROADCAST,
+        JOIN_ROOM,
+        JOIN_ROOM_CALLBACK,
+        JOIN_ROOM_BROADCAST,
+        DELETE_ROOM,
+        DELETE_ROOM_CALLBACK,
+        DELETE_ROOM_BROADCAST,
+        NEW_PLAYER,
+        NEW_PLAYER_CALLBACK,
+        NEW_PLAYER_BROADCAST,
+        PLAYER_UPDATE_POSITION,
+        HOTKEY_PRESSED
+    };
 
-  static Protocol& getInstance();
+    static Protocol& getInstance();
 
-  void handleMessage(SmartBuffer& smartBuffer);
+    void handleMessage(SmartBuffer& smartBuffer);
 
-private:
-  Protocol();
-  ~Protocol();
-  Protocol(const Protocol&) = delete;
-  Protocol& operator=(const Protocol&) = delete;
+  private:
+    Protocol();
+    ~Protocol();
+    Protocol(const Protocol&) = delete;
+    Protocol& operator=(const Protocol&) = delete;
 
-  void handleDefault(SmartBuffer& smartBuffer);
-  void handleCreateRoomCallback(SmartBuffer& smartBuffer);
-  void handleCreateRoomBroadcast(SmartBuffer& smartBuffer);
-  void handleJoinRoomCallback(SmartBuffer& smartBuffer);
-  void handleJoinRoomBroadcast(SmartBuffer& smartBuffer);
-  void handleDeleteRoomCallback(SmartBuffer& smartBuffer);
-  void handleDeleteRoomBroadcast(SmartBuffer& smartBuffer);
-  void handleNewPlayerBroadcast(SmartBuffer& smartBuffer);
-  void handlePlayerUpdatePosition(SmartBuffer& smartBuffer);
+    void handleDefault(SmartBuffer& smartBuffer);
+    void handleCreateRoomCallback(SmartBuffer& smartBuffer);
+    void handleCreateRoomBroadcast(SmartBuffer& smartBuffer);
+    void handleJoinRoomCallback(SmartBuffer& smartBuffer);
+    void handleJoinRoomBroadcast(SmartBuffer& smartBuffer);
+    void handleDeleteRoomCallback(SmartBuffer& smartBuffer);
+    void handleDeleteRoomBroadcast(SmartBuffer& smartBuffer);
+    void handleNewPlayerBroadcast(SmartBuffer& smartBuffer);
+    void handlePlayerUpdatePosition(SmartBuffer& smartBuffer);
 };
